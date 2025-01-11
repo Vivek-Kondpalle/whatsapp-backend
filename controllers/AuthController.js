66ae5a3eed12889627a8082c7321dc66ab328901
vivekKondpalleOmnify <vivek@getomnify.com>
2025-01-12 01:15:00 +0530

    Profile setup done

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -94,3 +94,42 @@ export const getUserInfo = async (request, response, next) => {
         return response.status(500).send("Internal server error")
     }
 }
+
+export const updateProfile = async (request, response, next) => {
+    try {
+
+        const { userId } = request
+        const { firstName, lastName, color } = request.body
+
+        if(!firstName || !lastName){
+            return response.status(400).send("First name, Last name and color is required.")
+        }
+
+        const userData = await User.findByIdAndUpdate(
+            userId,
+            {
+                firstName,
+                lastName,
+                color,
+                profileSetup: true
+            },
+            { new: true, runValidators: true }
+        )
+        if(!userData){
+            return response.status(404).send("User not found");
+        }
+
+        return response.status(200).json({
+            id: userData.id,
+            email: userData.email,
+            profileSetup: userData.profileSetup,
+            firstName: userData.firstName,
+            lastName: userData.lastName,
+            image: userData.image,
+            color: userData.color,
+        })
+    } catch (error) {
+        console.log(error)
+        return response.status(500).send("Internal server error")
+    }
+}
