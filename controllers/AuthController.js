e747dfce6add15f151c73b764c13bae620835eb1
vivekKondpalleOmnify <vivek@getomnify.com>
2025-01-15 23:31:47 +0530

    Added logout route

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -193,3 +193,13 @@ export const removeProfileImage = async (request, response, next) => {
     return response.status(500).send("Internal server error");
   }
 };
+
+export const logOut = async (request, response, next) => {
+  try {
+    response.cookie("jwt", "", {maxAge: 1, secure: true, sameSite: ""})
+    return response.status(200).send("Logout successfull.");
+  } catch (error) {
+    console.log(error);
+    return response.status(500).send("Internal server error");
+  }
+};
