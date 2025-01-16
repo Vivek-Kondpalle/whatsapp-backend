cd5d719c808fd1ed670bfe52ac2fe97f9d47d977
vivekKondpalleOmnify <vivek@getomnify.com>
2025-01-16 22:32:18 +0530

    Search contact API done

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import mongoose from "mongoose";
 import authRoutes from "./routes/AuthRoutes.js";
+import contactsRoutes from "./routes/ContactRoute.js";
 
 dotenv.config();
 
@@ -55,6 +56,7 @@ app.use(express.json())
 
 
 app.use("/api/auth", authRoutes)
+app.use("/api/contacts", contactsRoutes)
 
 const server = app.listen(port, () => {
   console.log(" Server is running on port", port);
