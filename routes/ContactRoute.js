fd093c8796f21a42189dd9d099490613a02b34bc
vivekKondpalleOmnify <vivek@getomnify.com>
2025-01-20 21:45:18 +0530

    Get Contacts for DM list

--- routes/ContactRoute.js
+++ routes/ContactRoute.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { verifyToken } from "../middlewares/AuthMiddleware.js";
-import { searchContact } from "../controllers/ContactController.js";
+import { searchContact, getContactsForDMList } from "../controllers/ContactController.js";
 
 const contactsRoutes = Router()
 
 contactsRoutes.post('/search', verifyToken, searchContact)
+contactsRoutes.get('/get-contacts-for-dm', verifyToken, getContactsForDMList)
 
 export default contactsRoutes
