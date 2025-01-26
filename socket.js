88cff49f07e871cf36504623db321f26685e4f20
vivekKondpalleOmnify <vivek@getomnify.com>
2025-01-26 14:57:46 +0530

    Send channel message socket event done

--- socket.js
+++ socket.js
@@ -1,5 +1,6 @@
 import { Server as SocketIOServer } from 'socket.io'
 import Message from './models/MessagesModel.js'
+import Channel from './models/ChannelModel.js'
 
 const allowedOrigins = ['http://localhost:5173', 'http://127.0.0.1:5173'];
 
@@ -53,6 +54,34 @@ const setupSocket = (server) => {
         }
     }
 
+    const sendChannelMessage = async (message) => {
+        const { channelId, sender, content, messageType, fileUrl } = message
+
+        const createdMessage = await Message.create({sender, recipient: null, content, messageType, fileUrl, timestamp: new Date()})
+
+        const messageData = await Message.findById(createdMessage._id).populate("sender", "id email firstName lastName image color").exec();
+
+        await Channel.findByIdAndUpdate(channelId, { $push: { messages: messageData._id } })
+
+        const channel = await Channel.findById(channelId).populate("members")
+
+        const finalData = {...messageData._doc, channelId: channel._id}
+
+        if(channel && channel.members){
+            channel.members.forEach(member => {
+                const memberSocketId = userSocketMap.get(member._id.toString())
+                if(memberSocketId){
+                    io.to(memberSocketId).emit('recieve-channel-message', finalData)
+                }
+            })
+
+            const adminSocketId = userSocketMap.get(channel.admin._id.toString())
+            if(adminSocketId){
+                io.to(adminSocketId).emit('recieve-channel-message', finalData)
+            }
+        }
+    }
+
     io.on('connection', (socket) => {
         const userId = socket.handshake.query.userId
 
@@ -64,6 +93,7 @@ const setupSocket = (server) => {
         }
 
         socket.on('sendMessage', sendMessage)
+        socket.on('send-channel-message', sendChannelMessage)
         socket.on('disconnect', () => disconnect(socket))
     })
 }
